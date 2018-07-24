85f5145f59d2135e5d30a20d4234fe71d853cd54
Sarahxdoughty <sarahxdoughty@gmail.com>
2018-07-24 21:13:59 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -10,11 +10,9 @@ def change_rainbow_colors
   rainbow_colors[2] = "light_yellow"
   return rainbow_colors
 end
-puts change_rainbow_colors
 
 def add_colors
   rainbow_colors << "green"
   rainbow_colors << "blue"
   return rainbow_colors
 end
-puts add_colors
